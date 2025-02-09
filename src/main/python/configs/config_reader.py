f9e030ea33e9e371a81e2bfdfcf158e6cbefa175
morpheus9631 <morpheus.hsieh@gmail.com>
2025-02-09 11:22:03 +0800

    Add path_util.py

---
+++ src/main/python/configs/config_reader.py
@@ -0,0 +1,75 @@
+import json
+import os
+import yaml
+from typing import Any, Dict
+
+from utils import PathUtil
+
+class ConfigReader:
+    
+    def __init__(self, config_file: str):
+        """Initialize with the config file"""
+        if not os.path.exists(config_file):
+            raise FileNotFoundError(f"'{config_file}' not found.")
+        
+        self.config_file = config_file
+        self.config_data = self._load_config()
+        
+    # -------------------------------------------------------------------------
+        
+    def _load_config(self) -> dict:
+        """Load config based on file extension"""
+        file_extension = os.path.splitext(self.config_file)[1].lower()
+
+        if file_extension in ['.yaml', '.yml']:
+            return self._load_yaml()
+        elif file_extension == '.json':
+            return self._load_json()
+        else:
+            raise ValueError(f"Unsupported file type: {file_extension}")
+
+    # -------------------------------------------------------------------------
+    
+    def _load_yaml(self) -> dict:
+        """Load and parse YAML file"""
+        with open(self.config_file, 'r', encoding='utf-8') as file:
+            return yaml.safe_load(file)
+
+    # -------------------------------------------------------------------------
+        
+    def _load_json(self) -> dict:
+        """Load and parse JSON file"""
+        with open(self.config_file, 'r', encoding='utf-8') as file:
+            return json.load(file)
+
+    # -------------------------------------------------------------------------
+        
+    def get_config(self, key: str) -> Any:
+        """Get configuration value based on provided key"""
+        config = self.config_data
+        
+        for part in key.split('.'):
+            config = config.get(part)
+            if config is None:
+                raise KeyError(f"Key '{key}' not found in configuration")
+        return config
+
+    # -------------------------------------------------------------------------
+    
+    def get_data(self) -> Dict[str, Any]:
+        """Return the entire config data"""
+        return self.config_data
+
+    # -------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+    
+    root_path = PathUtil.get_root_path()
+    print(f"Root path: {root_path}")
+    
+    resources_path = PathUtil.get_resources_path()
+    print(f"\nResources path: {resources_path}")
+    
+    
+    
---
+++ src/main/python/utils/path_util.py
@@ -0,0 +1,92 @@
+import os
+import logging
+
+class PathUtil:
+    
+    _instance = None
+    _root_path = None
+
+    # -------------------------------------------------------------------------
+    
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(PathUtil, cls).__new__(cls)
+            cls._init_root_path()
+        return cls._instance
+
+    # -------------------------------------------------------------------------
+
+    @classmethod
+    def _init_root_path(cls):
+        """
+        Initialize the root path
+        """
+        if cls._root_path is None:
+            curr_path = os.path.dirname(os.path.abspath(__file__))
+            root_path_str = os.path.join(curr_path, '../../../../')
+            cls._root_path = os.path.abspath(root_path_str).lower()
+
+    # -------------------------------------------------------------------------
+
+    @classmethod
+    def get_root_path(cls) -> str:
+        """
+        Return the root path of the project
+        """
+        if cls._root_path is None:
+            cls._init_root_path()
+        return cls._root_path
+
+    # -------------------------------------------------------------------------
+
+    @classmethod
+    def get_resources_path(cls, path_type: str='main') -> str:
+        
+        valid_types = ['main', 'test']
+        
+        if path_type not in valid_types:
+            raise ValueError(f"Invalid type '{path_type}'.")
+        
+        path_parts = ['src', path_type, ',resources']
+        resources_path = os.path.join(cls._root_path, *path_parts)
+        
+        if not os.path.exists(resources_path):
+            raise FileNotFoundError(f"path {resources_path} does not exist.")
+        
+        return resources_path
+
+    # -------------------------------------------------------------------------
+
+    @staticmethod
+    def ensure_path_exists(path: str, is_create: bool = True) -> bool | None:
+        """
+        Ensure the given path exists.
+
+        Returns:
+            None: if the path already exists.
+            bool: 
+                - True if the path was created.
+                - False if the path does not exist and was not created.
+        """
+        if os.path.exists(path):
+            return None
+            
+        if not is_create:
+            logging.warning(f"Path '{path}' does not exist.")
+            return False
+
+        try:
+            os.makedirs(path, exist_ok=True)
+            logging.info(f"Path '{path}' created.")
+            return True
+        except OSError as ex:
+            raise RuntimeError(f"Failed to create path '{path}': {ex}") from ex
+
+    # -------------------------------------------------------------------------
+
+
+if __name__=="__main__":
+    
+    root_path = PathUtil.get_root_path()
+    print(f"Root path: {root_path}")
+    
